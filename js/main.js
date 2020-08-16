570fe0d24e239d91faa36f84573db7c3c2c1667f
Sebastian Vargas <vargaspizango1@gmail.com>
2020-08-15 22:07:53 -0500

    Creación de la página web con elementos básicos añadidos

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var cy = cytoscape({
+    container: document.getElementById("grafo"),
+
+    elements: {
+        nodes: [
+            {data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
+        ],
+        /*edges: [
+            {data: {id: "e1", source: "A", target: "A"}}
+        ]*/
+    },
+
+    style: [
+        {
+            selector: "node",
+
+            style: {
+                "label": "data(id)"
+            }
+        }
+    ],
+
+    layout: {
+        name: "random",
+    }
+
+});
+
+
+cy.add({
+    nodes: [
+        {data: {id: "B"}, position: {x: Math.random(), y: Math.random()}}
+    ]
+})
+cy.fit();
