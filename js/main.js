22d57e612a526211e1a60f5fa2b6548ad70075e0
Sebastian Vargas <vargaspizango1@gmail.com>
2020-08-17 00:12:28 -0500

    Se agregó función de aparición de nodos

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ var cy = cytoscape({
     container: document.getElementById("grafo"),
 
     elements: {
-        nodes: [
+        /*nodes: [
             {data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
         ],
-        /*edges: [
+        edges: [
             {data: {id: "e1", source: "A", target: "A"}}
         ]*/
     },
@@ -28,10 +28,47 @@ var cy = cytoscape({
 
 });
 
+cy.remove(cy.$());
 
-cy.add({
+
+/*cy.add({
     nodes: [
         {data: {id: "B"}, position: {x: Math.random(), y: Math.random()}}
     ]
-})
-cy.fit();
+})*/
+
+function crearNodos(n = 0){
+    let i;
+    
+
+    if(n == null || n < 0){
+        console.log("Ingresaste algo mal...");
+    }else if(n === 0){
+        cy.remove(cy.$());
+    }else{
+        cy.remove(cy.$());
+        for(i = 0; i < n; i++){
+            cy.add({
+                nodes:
+                    [
+                        {data: {id: `${i + 1}`}}
+                    ]
+            })
+        }
+    }
+    cy.layout({
+        name: "random"
+    }).run();
+    cy.fit();
+}
+
+var formulario = document.querySelector(".ingreso-de-datos");
+
+formulario.addEventListener("submit", () =>{
+    var numeroDeNodos = parseInt(document.querySelector("#numero-nodos").value);
+    console.log(numeroDeNodos);
+
+    crearNodos(numeroDeNodos);
+});
+
+//cy.fit();
