463bbaf94156f66ac50fbcc0da91a5d28e880b08
unknown <mau9977@gmail.com>
2020-08-19 15:00:26 -0500

    codigo que une aristas implementado

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ var cy = cytoscape({
     container: document.getElementById("grafo"),
 
     elements: {
+
         nodes: [
-            {data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
+            //{data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
         ],
+
         /*edges: [
             {data: {id: "e1", source: "A", target: "A"}}
         ]*/
@@ -28,10 +30,58 @@ var cy = cytoscape({
 
 });
 
+let number_nodes
+let ids = []
 
-cy.add({
-    nodes: [
-        {data: {id: "B"}, position: {x: Math.random(), y: Math.random()}}
-    ]
-})
+function createNodes()
+{
+    number_nodes = parseInt(prompt("Digite el numero de nodos: ",0));
+
+    for(var i = 0; i < number_nodes; i++)
+    {
+        cy.add({
+            nodes: [
+                {data: {id: `${i + 1}`}, position: {x: Math.random() * 500, y: Math.random() * 500}, weight: 100}
+            ]
+        })
+    }
+}
+
+function fillIdsArray()
+{
+    for(var i = 0; i < number_nodes; i++)
+    {
+        ids[i] = i + 1;
+    }
+}
+
+function joinEdges(times_joined)
+{
+    let current_id;
+    let random;
+    random = ids[Math.floor(Math.random() * ids.length)]
+    current_id = random;
+
+    for(var i = 0; i < number_nodes; i++)
+    {
+        random = ids[Math.floor(Math.random() * ids.length)]
+        cy.add({
+            edges: [
+                {data: {id: `${"e" + (i + (times_joined * number_nodes))}`, source: `${current_id}`, target: `${random}`}}
+            ]
+        })
+        current_id = random;
+        ids.splice(ids.indexOf(random), 1);
+    }
+}
+
+createNodes();
+
+for(var i = 0; i < 2; i++)
+{
+    fillIdsArray();
+    joinEdges(i);
+}
+
+
 cy.fit();
