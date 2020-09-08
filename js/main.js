9be19dcbc3113acafdb3093e8cb1170fbabb1281
Sebastian Vargas <vargaspizango1@gmail.com>
2020-09-08 16:12:29 -0500

    Rediseño visual del proyecto

--- js/main.js
+++ js/main.js
@@ -1,39 +1,43 @@
 "use strict";
 
 var cy = cytoscape({
-    container: document.getElementById("grafo"),
+  container: document.getElementById("grafo"),
 
-    elements: {
+  elements: {
 
-        nodes: [
-            //{data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
-        ],
-
-        /*edges: [
-            {data: {id: "e1", source: "A", target: "A"}}
-        ]*/
-    },
-
-    style: [
-        {
-            selector: "node",
-
-            style: {
-                "label": "data(id)",
-            }
-        },
-        {
-            selector: "edge",
-
-            style: {
-                "curve-style": "unbundled-bezier"
-            }
-        }
+    nodes: [
+      //{data: {id: "A"}, position: {x: Math.random(), y: Math.random()}}
     ],
 
-    layout: {
-        name: "random",
+    /*edges: [
+        {data: {id: "e1", source: "A", target: "A"}}
+    ]*/
+  },
+
+  style: [{
+      selector: "node",
+
+      style: {
+        "label": "data(id)",
+        "text-halign": 'center',
+        'text-valign': 'center',
+        'background-color': 'white'
+      }
+    },
+    {
+      selector: "edge",
+
+      style: {
+        "curve-style": "bezier",
+        'line-color': 'rgb(240, 240, 240)',
+        'opacity': '0.5'
+      }
     }
+  ],
+
+  layout: {
+    name: "random",
+  }
 
 });
 
@@ -41,66 +45,69 @@ let number_nodes
 let ids = []
 let edges_counter = 1;
 
-function createNodes(number_nodes)
-{
-    for(var i = 0; i < number_nodes; i++)
-    {
-        cy.add({
-            nodes: [
-                {data: {id: `${i + 1}`}, position: {x: Math.random() * 500, y: Math.random() * 500}, weight: 100}
-            ]
-        })
-    }
+function createNodes(number_nodes) {
+  for (var i = 0; i < number_nodes; i++) {
+    cy.add({
+      nodes: [{
+        data: {
+          id: `${i + 1}`
+        },
+        position: {
+          x: Math.random() * 500,
+          y: Math.random() * 500
+        },
+        weight: 100
+      }]
+    })
+  }
 }
 
-function fillIdsArray()
-{
-    for(var i = 0; i < number_nodes; i++)
-    {
-        ids[i] = i + 1;
-    }
+function fillIdsArray() {
+  for (var i = 0; i < number_nodes; i++) {
+    ids[i] = i + 1;
+  }
 }
 
-function joinEdges()
-{
-    let current_id;
-    let random;
+function joinEdges() {
+  let current_id;
+  let random;
+  random = ids[Math.floor(Math.random() * ids.length)]
+  current_id = random;
+
+  for (var i = 0; i < number_nodes; i++) {
     random = ids[Math.floor(Math.random() * ids.length)]
-    current_id = random;
+    if (adyacency_matrix[current_id - 1][random - 1] == 0) {
+      cy.add({
+        edges: [{
+          data: {
+            id: `${"e" + edges_counter}`,
+            source: `${current_id}`,
+            target: `${random}`
+          }
+        }]
+      })
 
-    for(var i = 0; i < number_nodes; i++)
-    {
-        random = ids[Math.floor(Math.random() * ids.length)]
-        if(adyacency_matrix[current_id - 1][random - 1] == 0)
-        {
-            cy.add({
-                edges: [
-                    {data: {id: `${"e" + edges_counter}`, source: `${current_id}`, target: `${random}`}}
-                ]
-            })
-
-            adyacency_matrix[current_id - 1] [random - 1] = 1;
-            edges_counter++;
-        }
-        current_id = random;
-        ids.splice(ids.indexOf(random), 1);
+      adyacency_matrix[current_id - 1][random - 1] = 1;
+      edges_counter++;
     }
+    current_id = random;
+    ids.splice(ids.indexOf(random), 1);
+  }
 }
 
 let adyacency_matrix
-function createGraph(n_nodes)
-{
-    cy.remove(cy.$());
-    createNodes(n_nodes);
-    adyacency_matrix = Array(n_nodes).fill(null).map(() => Array(n_nodes).fill(0));
 
-    for(var i = 0; i < 2; i++)
-    {
-        fillIdsArray();
-        joinEdges();
-    }
+function createGraph(n_nodes) {
+  cy.remove(cy.$());
+  createNodes(n_nodes);
+  adyacency_matrix = Array(n_nodes).fill(null).map(() => Array(n_nodes).fill(0));
 
-    cy.fit();
+  for (var i = 0; i < 2; i++) {
+    fillIdsArray();
+    joinEdges();
+  }
+
+  cy.fit();
 }
 
 var formulario = document.querySelector(".ingreso-de-datos");
