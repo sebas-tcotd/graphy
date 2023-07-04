0ff6510be32a1779b2773619f7cc1e35a94b957c
Sebastian Vargas <vargaspizango1@gmail.com>
2023-07-03 21:54:26 -0500

    feat: add basic layout for graph view

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { BottomBar, TopBar } from "./components";
+import { BottomBar, GraphView, TopBar } from "./components";
 
 function App() {
   return (
-    <main className="bg-gradient-to-tr from-[#2B3745] to-[#3A1B6C] min-h-screen">
+    <main className="bg-gradient-to-tr from-[#2B3745] to-[#3A1B6C] | min-h-screen | flex flex-col">
       <TopBar />
-      {/* Separación temporal */}
-      <div style={{ height: "100px" }}></div>
+      <GraphView />
       <BottomBar />
     </main>
   );
--- src/components/GraphView/index.tsx
+++ src/components/GraphView/index.tsx
@@ -1,3 +1,11 @@
+import "./index.css";
+
 export function GraphView() {
-  return <div>{/* Panel para el grafo */}</div>;
+  return (
+    <div className="graph-view flex items-center px-4 text-center | flex-1">
+      <span className="text-white/60 text-sm">
+        Click on the "Generate" button to create a random graph!
+      </span>
+    </div>
+  );
 }
